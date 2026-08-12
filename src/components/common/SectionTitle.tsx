40e08b499f4db7cf8de7a73bbc457bacb90c7776
emtiazahmedanik <emtiazahmedanik@users.noreply.github.com>
2026-08-12 22:06:51 +0000

    deploy: 601b1edaf05684aa9d066c8e6dcc3cfac41dcf1c

--- src/components/common/SectionTitle.tsx
+++
@@ -1,9 +0,0 @@
-const SectionTitle = ({ children }: Readonly<{ children: string }>) => {
-  return (
-    <h2 className="text-center mx-auto text-3xl/6 md:text-4xl/6 font-bold">
-      {children}
-    </h2>
-  );
-};
-
-export default SectionTitle;
--- src/types/blog.ts
+++
@@ -1,7 +0,0 @@
-export interface IBlogPost {
-  id: string;
-  title: string;
-  content: string;
-  createdAt: Date | string;
-  updatedAt: Date | string;
-}
